044d759121001cce1bf2dd87bef0226f579f68e7
Thakesh <thakesh.d@birger.com>
2026-05-08 13:54:19 +0400

    1

---
+++ python-hello_world/2-print_quiz.py
@@ -0,0 +1,25 @@
+print("Hello world!")
+
+print("Holberton school")
+
+print(f"{98} Battery street")
+
+print(f"{98} Battery street, {'San Francisco'}")
+
+a = "Python is cool"
+print(a[4])
+
+a = "Python is cool"
+print(a[0:6])
+
+a = "Python is cool"
+print(a[:6])
+
+a = "Python is cool"
+print(a[7:])
+
+a = "Python is cool"
+print(a[7:-5])
+
+a = "Python is cool"
+print(a[-2])
---
+++ python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+str1 = str * 3
+print(str * 3) 
+print(str[:9])
