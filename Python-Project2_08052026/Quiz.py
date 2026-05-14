888f399437391adf855712e6c6e695642bcb4c4b
Thakesh <thakesh.d@birger.com>
2026-05-14 14:12:54 +0400

    p2_1

---
+++ Python-Project2_08052026/Quiz.py
@@ -0,0 +1,46 @@
+if True:
+    print("Holberton")
+else:
+    print("School")
+
+if 12 == 48/4:
+    print("Holberton")
+else:
+    print("School")
+
+if 12 == 48/4 and False:
+    print("Holberton")
+else:
+    print("School")
+    
+if 12 == 48/3 or 12 is 12:
+    print("Holberton")
+else:
+    print("School")
+    
+a = 12
+if a > 2:
+    if a % 2 == 0:
+        print("Holberton")
+    else:
+        print("C is fun")
+else:
+    print("School")    
+    
+a = 12
+if a < 2:
+    print("Holberton")
+elif a % 2 == 0:
+    print("C is fun")
+else:
+    print("School")
+    
+for i in range(4):
+    print(i, end=" ")
+    
+for z in range(2, 4):
+    print(z, end=" ")
+    
+for x in range(2, 10, 2):
+    print(x, end=" ")
+    
