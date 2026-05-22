346ce7e6e091ee85ba5234a781df08404c4ce015
Thakesh <thakesh.d@birger.com>
2026-05-22 16:20:14 +0400

    p8

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a square with a private instance attribute size."""
+
+
+class Square:
+    """Square class with private size attribute."""
+
+    def __init__(self, size):
+        self.__size = size
