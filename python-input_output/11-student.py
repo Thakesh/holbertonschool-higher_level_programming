71b56e634cebfae6a5a2dce7336995529e35a0f7
Thakesh <thakesh.d@birger.com>
2026-06-03 12:05:04 +0400

    p11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that defines a Student class."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dictionary representation of Student.
+
+        If attrs is a list of strings, only those attributes are returned.
+        Otherwise, all attributes are returned.
+        """
+        if isinstance(attrs, list) and all(isinstance(i, str) for i in attrs):
+            return {k: self.__dict__[k] for k in attrs if k in self.__dict__}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance."""
+        for key, value in json.items():
+            setattr(self, key, value)
