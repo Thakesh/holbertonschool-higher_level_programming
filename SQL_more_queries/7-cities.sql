548d96d9a2017ef2d6e223d5d7abd8aab2c469f3
Thakesh <thakesh.d@birger.com>
2026-06-05 14:27:34 +0400

    p14

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,17 @@
+-- Creates database hbtn_0d_usa and tables states and cities
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
+);
