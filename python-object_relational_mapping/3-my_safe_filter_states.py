8593806edf6860e49ae5595831c735235a72aba9
Thakesh <thakesh.d@birger.com>
2026-06-15 10:53:14 +0400

    p15

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Lists all states matching the given name safely."""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cur = db.cursor()
+
+    query = (
+        "SELECT * FROM states "
+        "WHERE BINARY name = %s "
+        "ORDER BY id ASC"
+    )
+
+    cur.execute(query, (sys.argv[4],))
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
