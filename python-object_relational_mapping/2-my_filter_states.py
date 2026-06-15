6de421d729b83eb96b722cc45875a83d25c946a9
Thakesh <thakesh.d@birger.com>
2026-06-15 10:49:50 +0400

    p15

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -4,7 +4,6 @@
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
@@ -16,7 +15,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(
         sys.argv[4]
     )
 
