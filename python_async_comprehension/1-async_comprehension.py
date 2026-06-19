73bee4af8c1e4730778571ad925b4499117542fc
Thakesh <thakesh.d@birger.com>
2026-06-19 13:55:09 +0400

    p21

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Async comprehension over async_generator."""
+
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Collect 10 random numbers from async_generator."""
+    return [num async for num in async_generator()]
